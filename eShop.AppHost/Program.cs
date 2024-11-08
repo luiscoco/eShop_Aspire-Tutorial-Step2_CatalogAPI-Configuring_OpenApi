d7e3f0f1a656faa3c48ce4d1cbea609bc1e7e640
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-11-08 14:38:24 +0100

    Add project files.

---
+++ eShop.AppHost/Program.cs
@@ -0,0 +1,13 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres")
+    .WithImage("ankane/pgvector")
+    .WithImageTag("latest")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var catalogDb = postgres.AddDatabase("catalogdb");
+
+var catalogApi = builder.AddProject<Projects.Catalog_API>("catalog-api")
+    .WithReference(catalogDb);
+
+builder.Build().Run();
